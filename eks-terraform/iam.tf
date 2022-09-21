1fc660a686aaea610ad30e854a3847b5bc7880c0
naoki-urabe <na0k1ura6e@gmail.com>
2022-09-21 20:57:24 +0900

    initial commit

---
+++ eks-terraform/iam.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_role" "eks-master" {
+  name               = "eks-master-role"
+  assume_role_policy = <<POLICY
+    {
+        "Version": "2012-10-17"
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "eks.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+    POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-master.name
+}
+
+resource "aws_iam_role" "eks-node" {
+  name               = "eks-node-role"
+  assume_role_policy = <<POLICY
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "ec2.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+    POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-worker-node" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "ecr-ro" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cni" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks-node.name
+}
+
+resource "aws_iam_instance_profile" "eks-node" {
+  name = "eks-node-profile"
+  role = aws_iam_role.eks-node.name
+}
